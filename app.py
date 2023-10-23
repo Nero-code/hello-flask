38fe30e141eb1afe07ecb427ee3cc6787f65aa8e
= <=>
2023-10-23 19:43:01 +0200

    python test cicd with firebase secret

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
+import firebase_admin
 from flask import Flask, render_template
+from firebase_admin import credentials
+from firebase_admin import db
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 app = Flask(__name__)
+
+
+cred = credentials.Certificate(os.getenv("FIREBASE_KEYS"))
+default_app = firebase_admin.initialize_app(cred, {
+    "databaseURL": "https://hello-flask-896f8-default-rtdb.firebaseio.com/"
+})
 
 
 @app.route('/')
